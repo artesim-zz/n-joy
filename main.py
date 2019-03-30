5d346aea6ba04e9aea72cfde960012fb1cb4f5ba
Artesim <artesim1852@gmail.com>
2019-03-30 17:41:18 +0100

    Also wiring the output node into main()

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import zmq
 
 from njoy_core.input_node import EmbeddedInputNode
+from njoy_core.output_node import EmbeddedOutputNode
 from njoy_core.core import Core
 
 
@@ -16,11 +17,17 @@ def main():
                                    events_endpoint="inproc://input_events",
                                    requests_endpoint="inproc://requests")
 
+    output_node = EmbeddedOutputNode(context=ctx,
+                                     events_endpoint="inproc://output_events",
+                                     requests_endpoint="inproc://requests")
+
     core.start()
     input_node.start()
+    output_node.start()
 
     core.join()
     input_node.join()
+    output_node.join()
 
 
 if __name__ == "__main__":
